2d692cea1a36a48ceb016c3a2da512c848dffa1c
dan-mutua <danmutua15@gmail.com>
2024-07-13 23:20:21 +0300

    add docker

--- go.mod
+++ go.mod
@@ -5,10 +5,11 @@ go 1.22.2
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/gorilla/mux v1.8.1
-	golang.org/x/crypto v0.19.0
-    github.com/lib/pq v1.10.9 
+	github.com/lib/pq v1.10.9
+	golang.org/x/crypto v0.25.0
 
 )
 
+require github.com/joho/godotenv v1.5.1 // indirect
 
 // require github.com/lib/pq v1.10.9 // indirect
--- main.go
+++ main.go
@@ -3,22 +3,28 @@ package main
 import (
   "database/sql"
   "fmt"
+  "log"
+  "os"
 
   _ "github.com/lib/pq"
-)
-
-const (
-  host     = "localhost"
-  port     = 5432
-  user     = "postgres"
-  password = "your-password"
-  dbname   = "calhounio_demo"
+  "github.com/joho/godotenv"
 )
 
 func main() {
-  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-    "password=%s dbname=%s sslmode=disable",
+  err := godotenv.Load()
+  if err != nil {
+    log.Fatalf("Error loading .env file")
+  }
+
+  host := os.Getenv("DB_HOST")
+  port := os.Getenv("DB_PORT")
+  user := os.Getenv("DB_USER")
+  password := os.Getenv("DB_PASSWORD")
+  dbname := os.Getenv("DB_NAME")
+
+  psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
     host, port, user, password, dbname)
+
   db, err := sql.Open("postgres", psqlInfo)
   if err != nil {
     panic(err)
